d613b482d37cfd257bd52369ff94f1f735982931
Kauany12389 <106532365+Kauany12389@users.noreply.github.com>
2024-07-26 11:45:23 -0300

    Add files via upload

--- MISSAO-IA-main/MISSAO-IA-main/script.js
+++ MISSAO-IA-main/MISSAO-IA-main/script.js
@@ -11,64 +11,26 @@ const caixaResultado = document.querySelector('.caixa-resultado');
  const perguntas = [
     {
     enunciado: "Em que lugar teve origem a palavra música?",
-    alternativas: [ 
-      {
-
-       texto: "Grécia",
+    alternativas: [
+    {
+    
+        texto:"Grécia",
         afirmação: "Afirmação da alternativa 1"
         },
-
-        texto: "China e Índia",
+        
+        {texto: "China e india",
         afirmação: "Afirmação da alternativa 2"
       },
-
-        texto: "Brasil",
-        afirmação: "Afirmação da alternativa 3"
-      }
-     ]
-     {
     
-    enunciado: "Quais são os elementos que compõe a daça?",
-    alternativas: [,
-      {
-        texto: "contrapeso",
-        afirmação: "Afirmação da alternativa 1"
-       },
-
-       {texto: "Espaço",
-        afirmação: "Afirmação da alternativa 2"
-       },
-
-       texto: "Ordem e transferência",
-        afirmação: "Afirmação da alternativa 3"
-       },
+      texto: "Brasil",
+      afirmação: "Afirmação da alternativa 3"
+      },
       ]
-      
-     {
+
     enunciado: "O teatro é uma linguagem de que arte?",
-    alternativas: [,
-    {
-
-    texto: "Artes literárias",
-    afirmação: "Afirmação da alternativa 1"
+    alternativas: ["Artes literárias", "Artes cênicas","Artes visuais","Artes músicais"],
     },
-
-    {texto: "Artes cênicas",
-    afirmação: "Afirmação da alternativa 2"
-     },
-
-     {texto: "Artes Visuais",
-    afirmação: "Afirmação da alternativa 3"
-    },
-
-    texto: "Artes musicais",
-    afirmação: "Afirmação da alternativa 4"
-  },
-   ]
-
-  },
     ];
-
     let atual = 0;
     let perguntaAtual;
     function mostraPergunta() {
@@ -82,7 +44,6 @@ const caixaResultado = document.querySelector('.caixa-resultado');
     for (const alternativa  of perguntaAtual.alternativas) {
         const botaoAlternativas = document. createElement("button");
         botaoAlternativas.textContent = alternativa;
-        botaoAlternativas.addEventListener("click", () =>respostaSelecionada(alternativa);
         caixaAlternativas.appendChild(botaoAlternativas);
     
     }
