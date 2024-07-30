e8dff3a01165d327616a3b27764f54973284a888
Kauany12389 <106532365+Kauany12389@users.noreply.github.com>
2024-07-30 09:07:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,35 +11,81 @@ const caixaResultado = document.querySelector('.caixa-resultado');
  const perguntas = [
     {
     enunciado: "Em que lugar teve origem a palavra música?",
-    alternativas: [" Grécia", "China e Índia","Brasil"],
-    },
-    {
-    enunciado: "Quais são os elementos que compõe a daça?",
     alternativas: [
-      {
-        texto: “Alternativa 1”,
-        afirmação: “Afirmação da alternativa 1”
-        },
-        {
-    },
     {
-    enunciado: "O teatro é uma linguagem de que arte?",
-    alternativas: ["Artes literárias", "Artes cênicas","Artes visuais","Artes músicais"],
+    
+        texto: "Grécia",
+        afirmação:"Afirmação da alternativa 1"
+        },
+
+       { texto: "China e Índia",
+        afirmação:"Afirmação da alternativa 2"
+      },
+
+      {texto: "Brasil",
+      afirmação:"Afirmação da alternativa 3"
     },
+      ]
+
+          enunciado: "Quais são os elementos que compõe a daça?",
+    alternativas: [
+    {
+
+      texto: "Contrapeso",
+      afirmação:"Afirmação da alternativa 1"
+    },
+
+     {texto: "Espaço",
+     afirmação:"Afirmação da alternativa 2"
+    },
+
+:   {texto:"Ordem e tranência",
+    afirmação:"Afirmação da alternativa 3"
+    },    
+     ]
+
+   {
+
+    enunciado: "O teatro é uma linguagem de que arte?",
+    alternativas: [
+    },
+
+    texto: "Arte literárias",
+    afirmação:"Afirmação da alternativa 1"
+    },
+
+    {texto: "Artes cênicas",
+    afirmação:"Afirmação da alternativa 2"
+    },
+
+    {texto: "Artes Visuais",
+    afirmação:"Afirmação da alternativa 3"
+    },
+
+    {texto:"Artes musicasis",
+    afirmação:"Afirmação da alternativa 4"
+    },  
+  
     ];
     let atual = 0;
     let perguntaAtual;
-    function mostraPergunta() {
-        perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado;
-        mostraAlternativas();
-        }
-        mostraPergunta();
-
-      function mostraAlternativas() {}
-    for (const alternativa  of perguntaAtual.alternativas) {
-        const botaoAlternativas = document. createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
-    
+    let historiaFinal = "";
+    function mostraAlternativas() {}
+     if(atual>=perguntas.length) {
+      mostraresultado();
+      return;
     }
+
+
+
+      function respostaSelecionada(opcaoSelecionada) {
+        const afirmacoes = opcaoSelecionada.afirmação;
+        historiaFinal = afirmacoes;
+        atual++;
+        mostraPergunta();
+          caixaAlternativas.appendChild(botaoAlternativas);
+      }
+    
+      
+        
+     
