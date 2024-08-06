2ea64566ef6872445a9d5bef3041b825c9e9b859
Kauany12389 <106532365+Kauany12389@users.noreply.github.com>
2024-08-06 09:07:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 const caixaPrincipal = document.querySelector('.caixa- principal');
-
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-    
-const caixaAlternativas = document.querySelector('.caixa-alternativas');
-    
-const caixaResultado = document.querySelector('.caixa-resultado');
-    
- const textoResultado = document.querySelector('.texto-resultado');
-
- const perguntas = [
+const caixaAlternativas = document.querySelector('.caixa-alternativas');    
+const caixaResultado = document.querySelector('.caixa-resultado');  
+const textoResultado = document.querySelector('.texto-resultado');
+const perguntas = [
     {
-    enunciado: "Em que lugar teve origem a palavra música?",
+    enunciado: "Em que lugar teve a origem a palavra música",
     alternativas: [
     {
-    
-        texto: "China",
-        afirmação:" "
+    texto: "China",
+    afirmação:""
         },
-
        { 
+
         texto: "Gécia",
         afirmação:""
-      },
+       },
 
       {
         texto: "Brasil",
@@ -31,11 +25,10 @@ const caixaResultado = document.querySelector('.caixa-resultado');
       {
         texto: "Índia",
         afirmação:""
-    },
-    
-      ]
+    }]
     },
 
+      {
           enunciado: "Quais são os elementos que compõe a daça?",
           alternativas: [
     {
@@ -49,7 +42,7 @@ const caixaResultado = document.querySelector('.caixa-resultado');
      afirmação:""
     },
 
-:   {
+   {
 
     texto:"Ordem ",
     afirmação:""
@@ -57,17 +50,14 @@ const caixaResultado = document.querySelector('.caixa-resultado');
   {
     texto:"transfêrencia",
     afirmação:""
-  },
-  
-  
-     ]
+  }]
 
     },
   
-
+    {
     enunciado: "O teatro é uma linguagem de que arte?",
     alternativas: [
-    },
+  {
 
     texto: "Arte literárias",
     afirmação:""
@@ -86,44 +76,44 @@ const caixaResultado = document.querySelector('.caixa-resultado');
     {
       texto:"Artes musicasis",
     afirmação:""
-    },  
-  
-    ];
+    }]
+  },
+];
+
     let atual = 0;
     let perguntaAtual;
     let historiaFinal = "";
 
     function mostraAlternativas() {
-     if(atual>=perguntas.length) {
+     if(atual >= perguntas.length) {
       mostraresultado();
       return;
     }
-    perguntaAtual=perguntas[atual];
-    caixaPerguntas.textContent=perguntaAtual.enunciado;
-    caixaAlternativas.textContent="";
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
   }
 
-    mostraPergunta();
    function mostraAlternativas(){
     for(const alternativas of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement ("button");
       botaoAlternativas.textContent = alternativas.texto;
-      botaoAlternativas.addEventListener("click",()=>respostaSelecionada(alternativas))
+      botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativas))
       caixaAlternativas.appendChild(botaoAlternativas);
-
     }
   }
+
   function mostraresultado(){
- caixaPerguntas.textContent = "O cultural abrange vária áreas e dentre ellas temos a art, a música, o tetro,a dança e muitos outros. todas ela conversando entre si, foram um lindo espetáculo."
-textoResultado.textContent =
+  caixaPerguntas.textContent = "O cultural abrange vária áreas e dentre ellas temos a art, a música, o tetro,a dança e muitos outros. todas ela conversando entre si, foram um lindo espetáculo."
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
+  }
 
-    caixaAlternativas
-
-
-
-
-    
-      
-        
-     
+  function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmação;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+  }
+  mostraPergunta();
